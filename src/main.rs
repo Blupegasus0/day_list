d7cfdd39b2b38df1619b66032c726b8eb1e29afc
Amir Eastmond <amireastmond@gmail.com>
2024-05-13 19:52:12 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::io::Error;
+
+use console::Term;
+use dialoguer::Input;
+
+fn main() {
+
+}
+
+struct todo {
+    id: usize,
+    name: String,
+    notes: String,
+}
+
+
+fn add_todo() {
+    
+}
+
+fn remove_todo() -> Result<(), Error> {
+    todo!();
+}
+
+fn edit_todo() -> Result<(), Error> {
+    todo!();
+}
+
+fn save_daylist() -> Result<(), Error> {
+    todo!();
+}
+
+fn load_daylist() -> Result<(), Error> {
+    todo!();
+}
