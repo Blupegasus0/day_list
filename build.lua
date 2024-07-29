d70403a88732354e5314e46030fa24c00da15722
Amir Eastmond <amireastmond@gmail.com>
2024-07-28 21:08:23 -0400

    feat: complete first iteration of build script

--- build.lua
+++ build.lua
@@ -1,21 +1,44 @@
 #! /usr/bin/lua
 
+Dependencies_linux = {
+  "cargp",
+  "rustup",
+  "mingw-w64",
+}
+
 -- Create release version for Linux (current os)
-local handle_local_build = io.popen("cargo build --release")
+local local_build = "cargo build --release"
+local handle_local_build = io.popen(local_build)
+print("Running: " .. local_build)
 if handle_local_build ~= nil then
   local result = handle_local_build:read("*a")
-  print(result)
-  handle_local_build:close()
+  local success, exit_type, exit_code = handle_local_build:close()
+  if not success then
+    print("error: " .. exit_code .. "\n" .. result)
+  end
 end
 
 -- add windows target
-os.execute("rustup target add x86_64-pc-windows-gnu")
+local add_win_target = "rustup target add x86_64-pc-windows-gnu"
+local handle_add_target= io.popen(add_win_target)
+print("Running: " .. add_win_target)
+if handle_add_target ~= nil then
+  local result = handle_add_target:read("*a")
+  local success, exit_type, exit_code = handle_add_target:close()
+  if not success then
+    print("error: " .. exit_code .. "\n" .. result)
+  end
+end
 
 -- Create release version for Windows
-local handle_win_build = io.popen("cargo build --release --target x86_64-pc-windows-gnu")
+local win_build = "cargo build --release --target x86_64-pc-windows-gnu"
+local handle_win_build = io.popen(win_build)
+print("Running: " .. win_build)
 if handle_win_build ~= nil then
-  local result = handle:read("*a")
-  print(result)
-  handle_win_build:close()
+  local result = handle_win_build:read("*a")
+  local success, exit_type, exit_code = handle_win_build:close()
+  if not success then
+    print("error: " .. exit_code .. "\n" .. result)
+  end
 end
 
