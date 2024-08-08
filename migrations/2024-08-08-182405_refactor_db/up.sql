db84ed4b412081cf1781dd7f9ebce28066e9e48a
Amir Eastmond <kengen-bb@proton.me>
2024-08-08 20:01:31 +0000

    refactor: start over 2

--- migrations/2024-08-08-182405_refactor_db/up.sql
+++
@@ -1,9 +0,0 @@
--- Your SQL goes here
-CREATE TABLE todos (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    completed BOOLEAN NOT NULL,
-    notes TEXT,
-    date DATE NOT NULL
-);
-
