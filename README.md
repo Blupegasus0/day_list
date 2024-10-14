7099a979038e4e17bce569a5e80f9d7985b5a8ba
Amir <kengen-bb@proton.me>
2024-10-14 10:17:22 -0400

    refactor: begin removing diesel files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A TUI todo list app built in rust
 
 ## Installation Guide
+
+
+#### Dev Setup
+
--- migrations/2024-08-08-203232_create_todos/down.sql
+++
@@ -1,4 +0,0 @@
--- This file should undo anything in `up.sql`
-DROP TABLE todo;
-DROP TABLE label;
-DROP TABLE todo_label;
