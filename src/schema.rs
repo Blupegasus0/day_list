5b341f73bf6144ea5c91a0eed3eb2b0582053c78
Amir <kengen-bb@proton.me>
2024-10-29 14:58:17 -0400

    feat: begin to impl `Todo` functions

--- src/schema.rs
+++ src/schema.rs
@@ -23,4 +23,26 @@ pub mod schema {
         pub color: Option<String>,
         pub favorite_status: bool,
     }
+
+
+    impl Todo {
+        pub new(
+        title: String, description: Option<String>, date_created: NaiveDateTime,
+        date_due: Option<NaiveDateTime>, reminder_date: Option<NaiveDateTime>,
+        parent_todo: Option<i32>, priority: i32, project_id: Option<i32>
+    ) -> self {
+        Todo {
+            id: 100, // not sure how to handle auto_increment
+            title,
+            description,
+            date_created,
+            status: false,
+            date_due,
+            reminder_date,
+            parent_todo,
+            priority,
+            project_id,
+        }
+}
+    }
 }
