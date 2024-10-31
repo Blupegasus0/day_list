5c0ba75a578f2d46345960e67d197367c4658e39
Amir <kengen-bb@proton.me>
2024-10-31 11:50:39 -0400

    bug: fix `new()`, forgot 'fn' keyword

--- src/schema.rs
+++ src/schema.rs
@@ -26,23 +26,23 @@ pub mod schema {
 
 
     impl Todo {
-        pub new(
-        title: String, description: Option<String>, date_created: NaiveDateTime,
-        date_due: Option<NaiveDateTime>, reminder_date: Option<NaiveDateTime>,
-        parent_todo: Option<i32>, priority: i32, project_id: Option<i32>
-    ) -> self {
-        Todo {
-            id: 100, // not sure how to handle auto_increment
-            title,
-            description,
-            date_created,
-            status: false,
-            date_due,
-            reminder_date,
-            parent_todo,
-            priority,
-            project_id,
+        pub fn new(
+            title: String, description: Option<String>, date_created: NaiveDateTime,
+            date_due: Option<NaiveDateTime>, reminder_date: Option<NaiveDateTime>,
+            parent_todo: Option<i32>, priority: i32, project_id: Option<i32>
+        ) -> Self {
+            Todo {
+                todo_id: 100, // not sure how to handle auto_increment
+                title,
+                description,
+                date_created,
+                status: false,
+                date_due,
+                reminder_date,
+                parent_todo,
+                priority,
+                project_id,
+            }
         }
-}
     }
 }
