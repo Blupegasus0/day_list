30c4cf6f509550aeb127a30b29958836f9c93091
Amir <kengen-bb@proton.me>
2024-10-31 13:10:43 -0400

    test: run hard-coded queries to create and list todos

--- src/bin/test_tui.rs
+++ src/bin/test_tui.rs
@@ -30,49 +30,68 @@ async fn establish_connection() -> Result<MySqlPool, sqlx::Error> {
 }
 
 // Execure SELECT query on database to get todos
-async fn get_all_todos(pool: &MySqlPool) -> Result<Vec<Todo>, sqlx::Error> {
+async fn get_all_todos(conn_pool: &MySqlPool) -> Result<Vec<Todo>, sqlx::Error> {
 // All database functions must return a Result<T>
     let todos = sqlx::query_as!(Todo, "SELECT * FROM todo")
-        .fetch_all(pool)
+        .fetch_all(conn_pool)
     .await?;
     Ok(todos)
 }
 
+// In this mf we using query! to perform functions that do not return results
+// Not query_as! because that expects results
+async fn test_create_todo(conn_pool: &MySqlPool) -> Result<(), sqlx::Error> {
+    //let current_date = ifaoeihf;
+    sqlx::query!("INSERT INTO todo (title, description, date_created, status, date_due, reminder_date, parent_todo, priority, project_id) 
+        VALUES('test create', 'from rust',  CURRENT_DATE(),0, CURRENT_DATE(), CURRENT_DATE, 1, 1, NULL);")
+        .execute(conn_pool)
+        .await?;
+    Ok(())
+}
+
+async fn create_todo(conn_pool: &MySqlPool, title: String, 
+    description: String, date_due: Option<NaiveDateTime>, 
+    reminder_date: Option<NaiveDateTime>, parent_todo: Option<i32>, 
+    priority: i32, project_id: Option<i32>
+) -> Result<(), sqlx::Error> {
+    //let current_date = ifaoeihf;
+
+    /*
+    sqlx::query!("INSERT INTO todo (title, description, date_created, status, date_due, reminder_date, parent_todo, priority, project_id) 
+        VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);"
+        title, description, current_date, false, date_due, reminder_date, parent_todo, priority, project_id
+    )
+        .execute(conn_pool)
+        .await?;
+*/
+    Ok(())
+}
+
+//async fn search_todos(conn_pool: &MySqlPool, search_string: &String) -> Result<Vec<Todo>, sqlx::Error> {
+    //let todos_found = sqlx::query_as!(Todo, "SELECT * FROM todo WHERE title LIKE %()")
+//}
+
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     // Set up the database connection
-    let pool = establish_connection().await?;
+    let conn_pool = establish_connection().await?;
 
     // Fetch and display all todos
-    match get_all_todos(&pool).await {
+    match test_create_todo(&conn_pool, ).await {
+        Ok(_) => println!("Todo created!"),
+        Err(err) => eprintln!("Error fetching todos: {:?}", err),
+    }
+
+    match get_all_todos(&conn_pool).await {
         Ok(todos) => {
             for todo in &todos {
-                println!("{:?}", todo);
+                println!("{}", todo.todo_id);
             }
             if todos.len() == 0 { println!("No data in table"); }
         }
         Err(err) => eprintln!("Error fetching todos: {:?}", err),
     }
 
-    match get_all_todos(&pool).await {
-        Ok(todos) => {
-            for todo in &todos {
-                println!("{:?}", todo);
-            }
-            if todos.len() == 0 { println!("No data in table"); }
-        }
-        Err(err) => eprintln!("Error fetching todos: {:?}", err),
-    }
-
-    match get_all_todos(&pool).await {
-        Ok(todos) => {
-            for todo in &todos {
-                println!("{:?}", todo);
-            }
-            if todos.len() == 0 { println!("No data in table"); }
-        }
-        Err(err) => eprintln!("Error fetching todos: {:?}", err),
-    }
 
     Ok(())
 }
